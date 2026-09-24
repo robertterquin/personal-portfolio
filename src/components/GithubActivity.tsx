4b073c0d137472847218e3bcca6292c24d70cbbc
Robert Terquin Laqui <terquinlaqui@gmail.com>
2026-09-25 00:10:11 +0800

    style: set section label back to GitHub Activity

--- src/components/GithubActivity.tsx
+++ src/components/GithubActivity.tsx
@@ -156,7 +156,7 @@ export const GithubActivity: React.FC = () => {
         }}
       >
         <div className="activity-title-col">
-          <span className="section-label">Activity</span>
+          <span className="section-label">GitHub Activity</span>
           <h2 className="section-title">Open Source Contributions</h2>
         </div>
       </motion.div>
